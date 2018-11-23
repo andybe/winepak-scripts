434b999a542c9671c53a9637f995fb2267384172
andybe <andreas.benzler@gmail.com>
2018-11-23 07:59:32 +0100

    add flathub resources and documentation

--- winepak-sdk.sh
+++ winepak-sdk.sh
@@ -1,4 +1,16 @@
+###################################
+# install org.freedesktop base    #
+###################################
+flatpak install flathub org.freedesktop.Sdk/i386/1.6 org.freedesktop.Sdk/x86_64/1.6 org.freedesktop.Platform/i386/1.6 org.freedesktop.Platform/x86_64/1.6
+
+###################################
+# create local repository         #
+###################################
 flatpak --user remote-add --no-gpg-verify --if-not-exists winepak winepak-repo
+
+###################################
+# build the winepak stack         #
+###################################
 flatpak-builder --arch=i386 --ccache --keep-build-dirs --force-clean --repo=winepak-repo builds/winepak-sdk-images-i386  winepak-sdk-images/org.winepak.Sdk.yml
 flatpak-builder --arch=x86_64 --ccache --keep-build-dirs --force-clean --repo=winepak-repo builds/winepak-sdk-images-x86_64  winepak-sdk-images/org.winepak.Sdk.yml
 flatpak build-commit-from --verbose --src-ref=runtime/org.winepak.Sdk/i386/3.0 winepak-repo runtime/org.winepak.Sdk.Compat32/x86_64/3.0
